797a8665a7e9bc0a2433d70a8b3ab874d9c59006
k109 <k1181512.1@gmail.com>
2020-02-02 12:00:32 +0100

    created repository layer for loans

---
+++ src/main/java/com/example/microloanapp/repositories/LoanRepository.java
@@ -0,0 +1,7 @@
+package com.example.microloanapp.repositories;
+
+import com.example.microloanapp.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+}
