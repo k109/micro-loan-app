d04a4611b508d0debae28c459bd9445cd8177ded
k109 <k1181512.1@gmail.com>
2020-02-02 11:50:53 +0100

    initial commit

---
+++ src/main/java/com/example/microloanapp/MicroLoanAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.microloanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLoanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroLoanAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microloanapp/MicroLoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microloanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroLoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
