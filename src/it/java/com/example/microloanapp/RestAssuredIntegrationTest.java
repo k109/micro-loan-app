99b93d668b01f8d8c4a8cda80ba0cb5f89890160
k109 <k1181512.1@gmail.com>
2020-02-03 08:23:32 +0100

    modified acceptance test

--- src/it/java/com/example/microloanapp/RestAssuredIntegrationTest.java
+++ src/it/java/com/example/microloanapp/RestAssuredIntegrationTest.java
@@ -1,21 +1,42 @@
 package com.example.microloanapp;
 
+import com.example.microloanapp.services.LoanService;
 import com.example.microloanapp.v1.controllers.LoanController;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestAssuredIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private String uri;
+
+    @PostConstruct
+    public void init() {
+        uri = "http://localhost:" + port;
+    }
+
+    @MockBean
+    LoanService loanService;
 
     private static final String POST_URL = LoanController.BASE_URL;
 
     private static Map<String, String> payload = new HashMap<>();
-    private static final String ID = "1";
     private static final String AMOUNT = "4000";
     private static final String NUMBER_OF_MOUNTHS = "3";
 
@@ -28,11 +49,8 @@ public class RestAssuredIntegrationTest {
         given()
                 .contentType(ContentType.JSON)
                 .body(payload)
-                .post(POST_URL)
+                .post(uri + POST_URL)
                 .then()
-                .statusCode(201)
-                .body("id", equalTo(1))
-                .body("numberOfMonths", equalTo(3))
-                .body("requestedAmount", equalTo(4000));
+                .statusCode(201).log().all();
     }
 }
